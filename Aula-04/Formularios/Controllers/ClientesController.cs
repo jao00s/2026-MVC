81d14ac60938123641bd156b71046f59d86cddb4
jao00s <joaofcojau@gmail.com>
2026-08-19 16:04:10 -0300

    Desafio do Dipa

---
+++ Aula-04/Formularios/Controllers/ClientesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC2026;
+
+public class ClientesController : Controller
+{
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    
+}
+
